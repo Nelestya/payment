9bf3e91e248c897fbd3979120022e35fb0af41cf
Nelestya <tristand.pro@gmail.com>
2017-10-01 19:32:41 +0200

    add views canceled and done and update process

--- views.py
+++ views.py
@@ -1,24 +1,43 @@
+from decimal import Decimal
+from django.conf import settings
 from django.core.urlresolvers import reverse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from paypal.standard.forms import PayPalPaymentsForm
+from orders.models import Order
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
-def view_that_asks_for_money(request):
+@csrf_exempt
+def payment_done(request):
+    return render(request, 'payment/done.html')
+
+@csrf_exempt
+def payment_canceled(request):
+    return render(request, 'payment/canceled.html')
+
+def payment_process(request):
+    order_id = request.session.get('order_id')
+    order = get_object_or_404(Order, id=order_id)
+    host = request.get_host()
 
     # What you want the button to do.
     paypal_dict = {
-        "business": "receiver_email@example.com",
-        "amount": "10000000.00",
-        "item_name": "name of the item",
-        "invoice": "unique-invoice-id",
-        "notify_url": request.build_absolute_uri(reverse('paypal-ipn')),
-        "return_url": request.build_absolute_uri(reverse('your-return-view')),
-        "cancel_return": request.build_absolute_uri(reverse('your-cancel-view')),
-        "custom": "premium_plan",  # Custom command to correlate to some function later (optional)
+        "business": settings.PAYPAL_RECEIVER_EMAIL,
+        "amount": '%.2f' % order.get_total_cost().quantize(Decimal('.01')),
+        "item_name": 'Order {}'.format(order.id),
+        "invoice": str(order.id),
+        "currency_code": 'EUR',
+        "notify_url": 'http://{}{}'.format(host, reverse('paypal-ipn')), #request.build_absolute_uri(reverse('paypal-ipn')),
+        "return_url": 'http://{}{}'.format(host, reverse('payment:done')), #request.build_absolute_uri(reverse('your-return-view')),
+        "cancel_return": 'http://{}{}'.format(host, reverse('payment:canceled')), #request.build_absolute_uri(reverse('your-cancel-view')),
+
+        # "custom": "premium_plan",  # Custom command to correlate to some function later (optional)
     }
 
     # Create the instance.
     form = PayPalPaymentsForm(initial=paypal_dict)
-    context = {"form": form}
+    context = {"form": form,
+                "order": order}
+
     return render(request, "payment.html", context)
